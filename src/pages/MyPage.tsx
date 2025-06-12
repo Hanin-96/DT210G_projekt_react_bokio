b6c6971e92ac496df4e24fe8e23534565bc0ed72
Hanin Farhan <haninfarhan96@gmail.com>
2025-06-12 17:36:51 +0200

    Fixat liten bugg med laddning av myPage sida

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -33,38 +33,43 @@ function MyPage() {
 
     return (
         <>
-            <div style={{ maxWidth: "100rem", width: "100%", margin: "2rem auto" }}>
-                <h1 style={{ marginBottom: "1rem" }}>Min sida</h1>
-                <h2>Inloggad, {user?.username ? user.username : ""}</h2>
-            </div>
+            {
+                user && <div>
 
-            <div style={{ maxWidth: "100rem", width: "100%", margin: "0 auto" }}>
-                <h3>Mina recensioner</h3>
+                    <div style={{ maxWidth: "100rem", width: "100%", margin: "2rem auto" }}>
+                        <h1 style={{ marginBottom: "1rem" }}>Min sida</h1>
+                        <h2>Inloggad, {user?.username ? user.username : ""}</h2>
+                    </div>
 
-                {
-                    loading && <p style={{ color: "white", margin: "2rem auto 2rem auto" }}>Laddar in recensioner...</p>
-                }
-                {!loading && <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", gap: "4rem", justifyContent: "left", marginTop: "4rem" }}>
+                    <div style={{ maxWidth: "100rem", width: "100%", margin: "0 auto" }}>
+                        <h3>Mina recensioner</h3>
 
-                    {
-                        reviews && reviews?.length > 0 ? (
-                            reviews.map((review: Review) => (
-                                <MyPageReview
-                                    myPageReviewProp={review}
-                                    key={review._id}
-                                    bookTitleImgProp={
-                                        bookTitleImageList?.find(bookTitleImage => bookTitleImage.bookId === review.bookId) || { bookId: '', title: '', thumbnail: '' }
-                                    }></MyPageReview>
-                            ))
-                        ) :
-                            <p>{error}</p>
-                    }
+                        {
+                            loading && <p style={{ color: "white", margin: "2rem auto 2rem auto" }}>Laddar in recensioner...</p>
+                        }
+                        {!loading && <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", gap: "4rem", justifyContent: "left", marginTop: "4rem" }}>
+
+                            {
+                                reviews && reviews?.length > 0 ? (
+                                    reviews.map((review: Review) => (
+                                        <MyPageReview
+                                            myPageReviewProp={review}
+                                            key={review._id}
+                                            bookTitleImgProp={
+                                                bookTitleImageList?.find(bookTitleImage => bookTitleImage.bookId === review.bookId) || { bookId: '', title: '', thumbnail: '' }
+                                            }></MyPageReview>
+                                    ))
+                                ) :
+                                    <p>{error}</p>
+                            }
+                        </div>
+                        }
+
+
+
+                    </div>
                 </div>
-                }
-
-
-
-            </div>
+            }
         </>
     )
 }
